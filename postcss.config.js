6af50ab1438765cb9b1e08eb7594c5e2d4368219
Ayush Mokal <95966154+ayushmokal@users.noreply.github.com>
2024-12-20 17:57:20 +0530

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,9 @@
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
+
 export default {
-  plugins: {
-    tailwindcss: {
-      config: './tailwind.config.mjs'
-    },
-    autoprefixer: {},
-  },
+  plugins: [
+    tailwindcss,
+    autoprefixer,
+  ],
 }
